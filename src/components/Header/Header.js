7f984bf0def70c0a6b2d116ca149587653091cb4
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-02-07 00:27:35 +0530

    feat: Added Header section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import "./header.scss";
+import medalIcon from "../../assets/medal_icon.png";
+import headerBanner from "../../assets/header_banner.png";
+
+function Header() {
+  return (
+    <header className="header container">
+      <img
+        src={headerBanner}
+        alt="BIO-S Team Recruitment"
+        className="header__banner"
+      />
+      <h1 className="header__title">
+        <img src={medalIcon} alt="Medal Icon" className="header__title__icon" />
+        <span className="header__title__text">BIO-S Team Recruitment</span>
+      </h1>
+    </header>
+  );
+}
+
+export default Header
